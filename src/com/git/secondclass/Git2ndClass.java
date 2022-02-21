8a00f51d3ed818fcf2a35948a2bbfb4214ab71c1
denizborak95 <borakdnz@gmail.com>
2022-02-21 14:17:47 -0800

    this commit has all the files

---
+++ src/com/git/secondclass/Git2ndClass.java
@@ -0,0 +1,12 @@
+package com.git.secondclass;
+
+public class Git2ndClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to second class of git");
+		
+		
+	}
+
+}
